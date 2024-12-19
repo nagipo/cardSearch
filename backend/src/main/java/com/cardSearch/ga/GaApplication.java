65be1f03f2613b21f74b168f229aeb36f7dfd7d4
eric.po <ericpo2206@gmail.com>
2024-12-19 15:56:45 +0800

    init backend

---
+++ backend/src/main/java/com/cardSearch/ga/GaApplication.java
@@ -0,0 +1,13 @@
+package com.cardSearch.ga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaApplication.class, args);
+	}
+
+}
